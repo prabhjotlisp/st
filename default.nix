4281d64ddb25b1b0ccd1c772b4756852bc344f10
prabhjotlisp <prabhjotsingh.lisp@gmail.com>
2024-11-20 19:19:58 +0530

    good

---
+++ default.nix
@@ -0,0 +1,18 @@
+let
+  pkgs= import <nixpkgs> {};
+in
+{
+  st = pkgs.stdenv.mkDerivation {
+    pname = "st";
+    version = "1.0";
+
+    src = ./.;
+
+    buildInputs = [];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp st $out/bin
+    '';
+  };
+}
